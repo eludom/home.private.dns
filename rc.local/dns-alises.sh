b20b51a904b0ddd487b7ce9d2a1966862efc88b6
George Jones <gmj@pobox.com>
2020-11-16 05:10:53 -0500

    Updated help, warning and info messages

--- rc.local/dns-alises.sh
+++ rc.local/dns-alises.sh
@@ -1,19 +1,64 @@
-# functions for swapping out resolv.conf 
+# functions for swapping out resolv.conf
+#
+# Usage: resolv [show|list|help|NAME]
+#
+# NOTES
+#   This is Linux specific and uses chattr to keep systemd
+#   from overrwriting the resolv.conf file we want
+#
+
+SOURCE_DIR="${HOME}/etc"
 
 function resolv() {
 
-    conf=${1:-"NONE"}
+    conf=${1:-"help"}
 
-    if [ ! -f ${conf} ]; then
-        warn "$conf does not exit"
-        return
+    if [ "$conf" == "help" ]; then
+
+       cat <<EOF >/dev/stderr
+Usage: resolv [show|list|help|NAME]
+
+Examples:
+  resolv quad9 # switch to quad9 resolv.conf
+  resolv show  # show the current /etc/resolv.conf
+  resolv list  # list avalable $HOME/etc/resolv.conf.NAME options
+
+
+FILES:
+  This assumes that there are files in $HOME/etc/resolv.conf.NAME
+  such as
+
+    /etc/resolv.conf.quad9
+    /etc/resolv.conf.google
+    /etc/resolv.conf.homenet
+
+  which will be copied to /etc/resolv.conf
+EOF
+       return 0
+    fi
+
+    if [ "${conf}" == "show" ]; then
+       echo cat /etc/resolv.conf >/dev/stderr
+       cat /etc/resolv.conf
+       return 0
+    fi
+
+    if [ "${conf}" == "list" ]; then
+       ls -1 ${SOURCE_DIR}/resolv.conf.*
+       return 0
+    fi
+
+    COPY_THIS="${SOURCE_DIR}/resolv.conf.$conf" 
+       
+    if [ ! -f ${COPY_THIS} ]; then
+        echo "${COPY_THIS} does not exit" >/dev/stderr
+        return 1
     fi
     
     sudo chattr -i /etc/resolv.conf
-    sudo cp "${conf}" /etc/resolv.conf
+    sudo cp "${COPY_THIS}" /etc/resolv.conf
     sudo chattr +i /etc/resolv.conf # tell systemd to take a hike
+    echo copied "${COPY_THIS}" /etc/resolv.conf to /etc/resolv >/dev/stderr
+    return 0
 }
 
-alias resolv-ed="resolv ${HOME}/etc/resolv.conf.ed /etc/resolv.conf"
-alias resolv-rhubarb="resolv ${HOME}/etc/resolv.conf.rhubarb /etc/resolv.conf"
-alias resolv-quad9="resolv ${HOME}/etc/resolv.conf.quad9 /etc/resolv.conf"
